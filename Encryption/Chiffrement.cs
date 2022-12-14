a1dadc5a1983b6bf33883c4ba11aba39853b99c3
jevei <j.veillette@hotmail.ca>
2022-12-14 11:11:03 -0500

    setup de base

---
+++ Encryption/Chiffrement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Encryption
+{
+    static class Chiffrement
+    {
+        internal static bool Chiffrer(string message, string cle)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bool Dechiffrer(string message, string cle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Encryption/Program.cs
+++ Encryption/Program.cs
@@ -6,7 +6,25 @@ namespace Encryption
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            do
+            {
+                Console.WriteLine("Veuillez choisir l'action désiré.\n1-Chiffrer. \n2-Déchiffrer.\n3-Quitter.");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        Console.WriteLine("Quitté.");
+                        break;
+                    default:
+                        Console.WriteLine("Entrée invalide!");
+                        break;
+                }
+            } while (input != "3");
         }
     }
 }
